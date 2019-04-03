3db4a222015317e70a5757e331a50a8a54f5f6c0
Christopher Monsanto <chris@monsan.to>
2019-04-03 17:23:11 -0400

    does this work

--- test.py
+++ test.py
@@ -1,2 +1,17 @@
 
-print(sys.version)
+import os
+
+os.mkdir("test", 0o333)
+
+fd = os.open("test", os.O_DIRECTORY | 0x8000)
+# fd = os.open("test", os.O_DIRECTORY | os.O_PATH)
+
+fd2 = os.open("file", os.O_CREAT|os.O_WRONLY, dir_fd=fd)
+os.write(fd2, b"sup")
+os.close(fd2)
+
+os.rename("test", "test2")
+
+fd2 = os.open("file", os.O_RDONLY, dir_fd=fd)
+print(os.read(fd2, 100))
+os.close(fd2)
