3bdec9258a5b46d7971b43412d9abfb57963cd78
Christopher Monsanto <chris@monsan.to>
2019-04-04 05:36:29 -0400

    test

--- test.py
+++ test.py
@@ -3,15 +3,19 @@ import os
 
 os.mkdir("test", 0o333)
 
-fd = os.open("test", os.O_WRONLY | 0x8000) # worth a shot
+info = os.stat("test")
+path = "/.vol/" + info.st_dev + "/" + info.st_ino + "/"
+
 # fd = os.open("test", os.O_DIRECTORY | os.O_PATH)
 
-fd2 = os.open("file", os.O_CREAT|os.O_WRONLY, dir_fd=fd)
+#fd2 = os.open("file", os.O_CREAT|os.O_WRONLY, dir_fd=fd)
+fd2 = os.open(path + "file", os.O_CREAT|os.O_WRONLY)
 os.write(fd2, b"sup")
 os.close(fd2)
 
 os.rename("test", "test2")
 
-fd2 = os.open("file", os.O_RDONLY, dir_fd=fd)
+#fd2 = os.open("file", os.O_RDONLY, dir_fd=fd)
+fd2 = os.open(path + "file", os.O_RDONLY)
 print(os.read(fd2, 100))
 os.close(fd2)
